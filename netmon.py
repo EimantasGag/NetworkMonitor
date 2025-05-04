fe4e63a35b572262ddb0896ef613a563383d747a
EimantasGag <eimis.gagilas@gmail.com>
2025-05-04 16:19:00 +0300

    Displaying icons of procceses + Kodas tragiskas cia tiesiog checkpointas

--- netmon.py
+++ netmon.py
@@ -2,11 +2,18 @@ from websockets.sync.client import connect
 import pyshark
 import psutil
 import socket
+import win32gui
+import win32ui
+from PIL import Image
+import win32con
+from io import BytesIO
+import base64
 
 host_interface = 'Ethernet'
 host_mac = None
 
 cachedPorts = {} # port -> process name
+processIconsSent = set() # list of processes names that had their icon sent
 
 def get_mac_address(interface_name):
     addrs = psutil.net_if_addrs()
@@ -20,7 +27,7 @@ def get_hostname_by_ip(ip_address):
         hostname, _, _ = socket.gethostbyaddr(ip_address)
         return hostname
     except socket.herror:
-        return "Hostname not found"
+        return None
 
 def find_pid_by_port(port):
     port = int(port)
@@ -29,6 +36,13 @@ def find_pid_by_port(port):
             if conn.pid is not None:
                 return conn.pid
     return None
+
+def get_executable_path_by_pid(pid):
+    try:
+        process = psutil.Process(pid)
+        return process.exe()
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
+        return None
 
 def find_processname_by_pid(pid):
     if pid is not None:
@@ -39,23 +53,100 @@ def find_processname_by_pid(pid):
             return None
     return None
 
-def create_message(packet, type):
-    packet_mac = packet.eth.dst
+def get_port(packet, direction):
+    if hasattr(packet, 'tcp'):
+        if direction == 'src':
+            return packet.tcp.srcport
+        elif direction == 'dst':
+            return packet.tcp.dstport
+    elif hasattr(packet, 'udp'):
+        if direction == 'src':
+            return packet.udp.srcport
+        elif direction == 'dst':
+            return packet.udp.dstport
+    return None
+
+def get_icon_from_exe(exe_path):
+    try:
+        # Load icon from file
+        _, small = win32gui.ExtractIconEx(exe_path, 0)
+        if not small:
+            return None
+
+        # Use the large icon if available, else small
+        hicon = small[0]
+
+        # Get icon info
+        hdc = win32ui.CreateDCFromHandle(win32gui.GetDC(0))
+        hbmp = win32ui.CreateBitmap()
+        hbmp.CreateCompatibleBitmap(hdc, 256, 256)
+
+        hdc_mem = hdc.CreateCompatibleDC()
+        hdc_mem.SelectObject(hbmp)
+        win32gui.DrawIconEx(hdc_mem.GetHandleOutput(), 0, 0, hicon, 256, 256, 0, None, win32con.DI_NORMAL)
+
+        bmpinfo = hbmp.GetInfo()
+        bmpstr = hbmp.GetBitmapBits(True)
+
+        image = Image.frombuffer(
+            'RGB',
+            (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+            bmpstr, 'raw', 'BGRX', 0, 1
+        )
+        #image = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+        return image
+
+    except Exception as e:
+        return None
+    
+def is_receiving_packet(packet):
+    return packet.eth.dst == host_mac
+
+def create_icon_message(port):
+    pid = find_pid_by_port(port)
+
+    if port not in cachedPorts:    
+        proc_name = find_processname_by_pid(pid)
+        cachedPorts[port] = proc_name
+    else:
+        proc_name = cachedPorts[port]
+
+    if proc_name in processIconsSent:
+        return None
+
+    exe_path = get_executable_path_by_pid(pid)
+    icon = get_icon_from_exe(exe_path)
+    processIconsSent.add(proc_name)
+
+    if icon is None:
+        return None
+    else:
+        # Convert the Image to a base64 string
+        buffer = BytesIO()
+        icon.save(buffer, format="PNG")
+        icon_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+        return f"I {proc_name} {icon_base64} X"
+
+# Function can only be called for tcp and udp packets
+def create_message(packet):
     length = int(packet.frame_info.len)
     
     # Receiving packets
-    if packet_mac == host_mac:
-        port = int(packet.tcp.dstport) if type == 'tcp' else int(packet.udp.dstport)
+    if is_receiving_packet(packet):
+        port = get_port(packet, 'dst')
 
         if port not in cachedPorts:
             proc_name = find_processname_by_pid(find_pid_by_port(port))
 
             # If a packet is received on port 80 or 443, get the hostname by IP
-            if type == 'tcp' and (packet.tcp.srcport == "80" or packet.tcp.srcport == "443"):
+            if get_port(packet, 'src') == "80" or get_port(packet, 'src') == "443":
                 proc_name = get_hostname_by_ip(packet.ip.src)
-            elif type == 'udp' and (packet.udp.srcport == "80" or packet.udp.srcport == "443"):
-                proc_name = get_hostname_by_ip(packet.ip.src)
+                if proc_name is None:
+                    proc_name = packet.ip.src
 
+            print("Process path: ", get_executable_path_by_pid(find_pid_by_port(port)))
             cachedPorts[port] = proc_name
         else:
             proc_name = cachedPorts[port]
@@ -66,16 +157,16 @@ def create_message(packet, type):
             msg = f"R {port} {proc_name.replace(" ", "_")} {length}"
     # Sending packets
     else: 
-        port = int(packet.tcp.srcport) if type == 'tcp' else int(packet.udp.srcport)
+        port = get_port(packet, 'src')
 
         if port not in cachedPorts:
             proc_name = find_processname_by_pid(find_pid_by_port(port))
 
             # If a packet is received on port 80 or 443, get the hostname by IP
-            if type == 'tcp' and (packet.tcp.dstport == "80" or packet.tcp.dstport == "443"):
+            if get_port(packet, 'dst') == "80" or get_port(packet, 'dst') == "443":
                 proc_name = get_hostname_by_ip(packet.ip.dst)
-            elif type == 'udp' and (packet.udp.dstport == "80" or packet.udp.dstport == "443"):
-                proc_name = get_hostname_by_ip(packet.ip.dst)
+                if proc_name is None:
+                    proc_name = packet.ip.dst
 
             cachedPorts[port] = proc_name
         else:
@@ -91,21 +182,25 @@ def create_message(packet, type):
 def resolve_packets(capture, websocket):
     for packet in capture.sniff_continuously():
         try:
-            if hasattr(packet, 'tcp'):
-                msg = create_message(packet, 'tcp')
-            elif hasattr(packet, 'udp'):
-                msg = create_message(packet, 'udp')
+            if hasattr(packet, 'tcp') or hasattr(packet, 'udp'):
+                msg = create_message(packet)
+
+                if is_receiving_packet(packet):
+                    icon_msg = create_icon_message(get_port(packet, 'dst'))
+                else:
+                    icon_msg = create_icon_message(get_port(packet, 'src'))
             else:
                 msg = None
         except AttributeError:
             msg = None
 
-        if msg is not None:
-            try:
+        try:
+            if msg is not None:
                 websocket.send(msg)
-            except Exception as e:
-                print(f"[-] Error sending message: {e}")
-                return
+            if icon_msg is not None:
+                websocket.send(icon_msg)
+        except Exception as e:
+            print(f"[-] Error sending message: {e}")
             
 def main():
     uri = "ws://localhost:50558/python"
